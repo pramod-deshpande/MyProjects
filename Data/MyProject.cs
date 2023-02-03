1031d473a9ba081f770ee9ab43f1ec980a7c1b89
Pramod <emailforcoding123@gmail.com>
2023-02-03 10:02:10 -0500

    Finished creating the form to add a new project, need to hook the form the method to save the Data

--- Data/MyProject.cs
+++ Data/MyProject.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyProjects.Data
 {
@@ -6,11 +7,13 @@ namespace MyProjects.Data
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [Required]
         public string ProjectName { get; set; }
         public string Label { get; set; }
         public string Description { get; set; }
         public string Notes { get; set; }
         public string GithubLink { get; set; }
+        [Required]
         public string Category { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime DraftCompletionDate { get; set; }
